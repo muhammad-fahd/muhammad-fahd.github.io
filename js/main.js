9727bc31325f0a1fd94e0126c9a256c31a592d45
Muhammad Fahd <malikfahdnadeem@gmail.com>
2023-04-11 17:23:49 +0500

    date updated

--- js/main.js
+++ js/main.js
@@ -242,7 +242,7 @@ $(document).ready(function() {
   //------- Timer Countdown  js --------//
 
   if (document.getElementById("count")) {
-    var countDownDate = new Date("March 19, 2020 15:30:00").getTime();
+    var countDownDate = new Date("May 20, 2023 18:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
